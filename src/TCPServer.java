b36c764727c5caa922bdb4d67db7d6673dd5eaad
Tony Le <lvtien.dev@gmail.com>
2013-03-09 21:23:19 +0800

    update

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -17,20 +17,11 @@ public class TCPServer {
 	public static OutputStream os;
 	public static DataOutputStream output;
 
-	public static void handleGETRequest(String firstLine) throws Exception {
-		String fields[] = firstLine.split(" ");
-		String fileName = fields[1];
-		String filePath = folderPath + fileName;
-		System.out.println(fileName);
 
-		// Read the rest of header
-		String inputString = firstLine;
-		while (inputString.compareTo("") != 0) {
-			inputString = isbr.readLine();
-		}
-
-		File f = new File(filePath);
-		if (f.canRead()) {
+	public static void handleStaticFile(String fileName) {
+		try {
+			String filePath = folderPath + fileName;
+			File f = new File(filePath);
 			int size = (int) f.length();
 			byte buffer[] = new byte[size];
 			FileInputStream fis = new FileInputStream(filePath);
@@ -39,33 +30,83 @@ public class TCPServer {
 			output.writeBytes("HTTP/1.0 200 OK\r\n");
 			if (fileName.endsWith("html")) {
 				output.writeBytes("Content-Type: text/html\r\n");
-				output.writeBytes("\r\n");
-				output.write(buffer, 0, size);
 			} else if (fileName.endsWith("jpg")) {
 				output.writeBytes("Content-Type: image/jpeg\r\n");
-				output.writeBytes("\r\n");
-				output.write(buffer, 0, size);
-			} else if (fileName.endsWith("pl")) {
-				String requestMethod = "REQUEST_METHOD=GET";
-				String queryString = "QUERY_STRING=\"\"";
-				String contentType = "";
-				String contentLengthString = "";
-				String env = requestMethod + " " + queryString + " " + contentType
-						+ " " + contentLengthString;
-				Process p = Runtime.getRuntime().exec("/usr/bin/env " + env +
-						" /usr/bin/perl " + filePath);
+			} else if (fileName.endsWith("gif")) {
+				output.writeBytes("Content-Type: image/gif\r\n");
+			} else if (fileName.endsWith("css")) {
+				output.writeBytes("Content-Type: text/css\r\n");
+			}
+
+			output.writeBytes("\r\n");
+			output.write(buffer, 0, size);
+			output.writeBytes("\r\n");
+
+			fis.close();
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	public static String getFileNameFromString(String string) {
+		// Remove query strings for checking
+		String fileName = string;
+		int queryStringStartIndex = string.indexOf("?");
+		if (queryStringStartIndex > 0) {
+			fileName = string.substring(0, queryStringStartIndex);
+		}
+		return fileName;
+	}
+
+	
+	public static String getQueryString(String string) {
+		String queryString = "";
+		int queryStringStartIndex = string.indexOf("?");
+		if (queryStringStartIndex > 0) {
+			queryString = string.substring(queryStringStartIndex + 1);
+		}
+		return queryString;
+	}
+
+	public static void handleGETRequest(String firstLine) throws Exception {
+		String fields[] = firstLine.split(" ");
+		String fileName = getFileNameFromString(fields[1]);
+		String filePath = folderPath + fileName;
+		System.out.println(fileName);
+		
+		String queryString = "";
+		if (getQueryString(fields[1]) != "") {
+			queryString = "QUERY_STRING=" + getQueryString(fields[1]);
+		}
+		String requestMethod = "REQUEST_METHOD=GET";
+		
+		// Read the rest of header
+		String inputString = firstLine;
+		while (inputString.compareTo("") != 0) {
+			inputString = isbr.readLine();
+		}
+
+		File f = new File(filePath);
+		if (f.canRead()) {
+			if (fileName.endsWith("pl")) {
+				String env = requestMethod + " " + queryString;
+				Process p = Runtime.getRuntime().exec(
+						"/usr/bin/env " + env + " /usr/bin/perl " + filePath);
 				try {
+					output.writeBytes("HTTP/1.0 200 OK\r\n");
 					BufferedReader br = new BufferedReader(
 							new InputStreamReader(p.getInputStream()));
 					String line;
-					while ((line = br.readLine()) != null)
+					while ((line = br.readLine()) != null) {
 						output.writeBytes(line + "\r\n");
+					}
+					output.writeBytes("\r\n");
 				} catch (Exception e) {
 					System.out.println("Command failed!");
 				}
-				output.writeBytes("\r\n");
+			} else {
+				handleStaticFile(fileName);
 			}
-			fis.close();
 		} else {
 			output.writeBytes("HTTP/1.0 404 Not Found\r\n");
 			output.writeBytes("\r\n");
@@ -76,6 +117,8 @@ public class TCPServer {
 		String fields[] = firstLine.split(" ");
 		String fileName = fields[1];
 		String filePath = folderPath + fileName;
+		
+		String contentType = "";
 
 		int contentLength = -1;
 		while (true) {
@@ -84,6 +127,9 @@ public class TCPServer {
 			if (line.startsWith(contentLengthStr)) {
 				contentLength = Integer.parseInt(line
 						.substring(contentLengthStr.length()));
+			} else if (line.startsWith("Content-Type:")) {
+				String contentLengthFields[] = line.split(" ");
+				contentType = "CONTENT_TYPE=" + contentLengthFields[1];
 			}
 
 			if (line.length() == 0) {
@@ -94,19 +140,23 @@ public class TCPServer {
 		// We should actually use InputStream here, but let's assume bytes map
 		// to characters
 		final char[] content = new char[contentLength];
-		System.out.println("CONTENT LENGTH + " + contentLength);
 		isbr.read(content);
 		String contentString = new String(content);
+
 		String requestMethod = "REQUEST_METHOD=POST";
-		String queryString = "QUERY_STRING=" + contentString;
-		String contentType = "CONTENT_TYPE=application/x-www-form-urlencoded";
-		String contentLengthString = "CONTENT_LENGTH=" + contentLength;
-		String env = requestMethod + " " + queryString + " " + contentType
+		String contentLengthString = "CONTENT_LENGTH="
+				+ contentLength;
+		String env = requestMethod + " " + contentType
 				+ " " + contentLengthString;
-//		String env = requestMethod;
+
 		System.out.println(env);
 		Process p = Runtime.getRuntime().exec(
 				"/usr/bin/env " + env + " /usr/bin/perl " + filePath);
+
+		DataOutputStream o = new DataOutputStream(p.getOutputStream());
+		o.writeBytes(contentString + "\r\n");
+		o.close();
+
 		try {
 			output.writeBytes("HTTP/1.0 200 OK\r\n");
 			BufferedReader br = new BufferedReader(new InputStreamReader(
@@ -127,23 +177,35 @@ public class TCPServer {
 		return tokens[tokens.length - 1];
 	}
 
-	public static String getDescription(String inputString) {
-		String descriptionString = "description=";
-		if (inputString.startsWith(descriptionString)) {
-			String string = inputString.substring(descriptionString.length());
-			int index = string.lastIndexOf("action=");
-			return string.substring(0, index - 1);
-		} else {
-			return null;
+	public static String getBodyRequest() throws Exception {
+		int contentLength = -1;
+		while (true) {
+			final String line = isbr.readLine();
+			final String contentLengthStr = "Content-Length: ";
+			if (line.startsWith(contentLengthStr)) {
+				contentLength = Integer.parseInt(line
+						.substring(contentLengthStr.length()));
+			}
+
+			if (line.length() == 0) {
+				break;
+			}
 		}
+
+		// We should actually use InputStream here, but let's assume bytes map
+		// to characters
+		final char[] content = new char[contentLength];
+		isbr.read(content);
+		String contentString = new String(content);
+		return contentString;
 	}
 
 	public static void main(String[] args) throws Exception {
 		int firstArg;
 		if (args.length > 0) {
-		    try {
-		        firstArg = Integer.parseInt(args[0]);
-		        serverSocket = new ServerSocket(firstArg);
+			try {
+				firstArg = Integer.parseInt(args[0]);
+				serverSocket = new ServerSocket(firstArg);
 				while (true) {
 					System.out.println("waiting for new connection");
 					clientSocket = serverSocket.accept();
@@ -168,10 +230,10 @@ public class TCPServer {
 					clientSocket.close();
 					System.out.println("connection closed");
 				}
-		    } catch (NumberFormatException e) {
-		        System.err.println("Argument" + " must be an integer");
-		        System.exit(1);
-		    }
+			} catch (NumberFormatException e) {
+				System.err.println("Argument" + " must be an integer");
+				System.exit(1);
+			}
 		}
 	}
 
