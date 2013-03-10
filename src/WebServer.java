3e002de5a2c0ddfc537aff7bf6f5932f9731e4b2
Tony Le <lvtien.dev@gmail.com>
2013-03-10 12:55:08 +0800

    update

--- src/WebServer.java
+++ src/WebServer.java
@@ -3,10 +3,8 @@ import java.util.*;
 import java.net.*;
 
 public class WebServer {
-
-	// public static String folderPath = "/Users/victor/a1";
-	public static String folderPath = "/Users/leviettien/a1";
-	// public static String folderPath = "/home/a008/a0088447/a1";
+	
+	public static final String folderPath = System.getProperty("user.dir") + "/a1";
 
 	public static int socketNumber = 2102;
 
